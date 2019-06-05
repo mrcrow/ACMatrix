6dae29d5a751bf40b0e4a7196f6b016605336461
WU Wenzhi <wwz.michael@gmail.com>
2019-06-06 00:33:27 +0800

    commit

---
+++ Example/MatrixTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatrixTest
+//
+//  Created by Wenzhi WU on 26/5/2019.
+//  Copyright © 2019 Wenzhi WU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
